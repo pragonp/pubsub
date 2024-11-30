8b00754854d08484c568d107d644c362b2061c27
PragonP <pragonp@protonmail.com>
2024-11-30 15:09:41 +1100

    extract out the events to separate package

---
+++ events/MachineRefillEvent.ts
@@ -0,0 +1,13 @@
+import { IEvent } from './IEvent';
+
+class MachineRefillEvent implements IEvent {
+    constructor(private readonly _refill: number, private readonly _machineId: string) { }
+
+    machineId(): string {
+        throw new Error("Method not implemented.");
+    }
+
+    type(): string {
+        throw new Error("Method not implemented.");
+    }
+}
---
+++ events/MachineSaleEvent.ts
@@ -0,0 +1,17 @@
+import { IEvent } from './IEvent';
+
+class MachineSaleEvent implements IEvent {
+    constructor(private readonly _sold: number, private readonly _machineId: string) { }
+
+    machineId(): string {
+        return this._machineId;
+    }
+
+    getSoldQuantity(): number {
+        return this._sold
+    }
+
+    type(): string {
+        return 'sale';
+    }
+}
